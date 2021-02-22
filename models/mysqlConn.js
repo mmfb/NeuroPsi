3be4cf5780c105c0f63f108d4693742be27ecf5c
mmfb <miguel.bugalho@gmail.com>
2021-02-22 22:50:03 +0000

    Update mysqlConn.js
    Changed connetion details for a new database

--- models/mysqlConn.js
+++ models/mysqlConn.js
@@ -2,9 +2,9 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
     connectionLimit: 60,
     host: 'remotemysql.com',
-    user: 'iWt6KIcwKt',
-    password: 'plnWbvnRx7',
-    database: 'iWt6KIcwKt'
+    user: 'M9oxyXkESZ',
+    password: 'HWBnniWuKf',
+    database: 'M9oxyXkESZ'
 });
 
 exports.pool = pool;
